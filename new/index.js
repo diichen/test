df791bef1e9d00778225531cc5c9fa4bcc360f60
hc <diichen@163.com>
2012-12-02 14:53:37 +0800

    test4
    test4

--- new/index.js
+++ new/index.js
@@ -7,3 +7,13 @@ handle["/start"]=requestHandlers.start;
 handle["/upload"]=requestHandlers.upload;
 handle["/show"]=requestHandlers.show;
 server.start(router.route,handle);
+
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHanders");
+var handle = {}
+handle["/"]=requestHandlers.start;
+handle["/start"]=requestHandlers.start;
+handle["/upload"]=requestHandlers.upload;
+handle["/show"]=requestHandlers.show;
+server.start(router.route,handle);
